989e570bc7b5431c5a91bb5174a90f8defd94b05
karan singh <41100240+krnsingh97@users.noreply.github.com>
2019-09-15 00:03:49 -0400

    Update app.js

--- hello/app.js
+++ hello/app.js
@@ -1,1 +1,8 @@
-console.log ('HEllo world!');
+var http= require('http');
+
+http.createServer(function(req, res){
+    res.writeHead(200, {'content-type':'text/plain'});
+    res.end('hello World!');
+}).listen(9001);
+
+console.log('server is running');
